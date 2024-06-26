3720e20fa7bb4c44b054f5e16d72b2927a300c61
Phati <phatisavant@gmail.com>
2024-06-27 00:40:52 +0530

    added configurable jar and workspace folder names

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,6 +4,6 @@ echo "*******************************"
 echo "***** Building Jar ************"
 echo "*******************************"
 
-WORKSPACE=/var/lib/jenkins/workspace/bill-pay-app
+WORKSPACE=/var/lib/jenkins/workspace/$JENKINS_JOB_NAME
 
 docker run --rm  -v $WORKSPACE:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3.8.3-openjdk-17 "$@"
--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,5 +4,5 @@ echo "*******************************"
 echo "***** Running Unit Tests ******"
 echo "*******************************"
 
-WORKSPACE=/var/lib/jenkins/workspace/bill-pay-app
+WORKSPACE=/var/lib/jenkins/workspace/$JENKINS_JOB_NAME
 docker run --rm  -v $WORKSPACE:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3.8.3-openjdk-17 "$@"
