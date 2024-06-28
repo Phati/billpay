1b8c1f2124dad33168fcb0e9f497fe9bc8261b1a
Phati Sawant <61258100+Phati@users.noreply.github.com>
2024-06-29 01:06:16 +0530

    Update mvn.sh

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,6 +4,4 @@ echo "*******************************"
 echo "***** Building Jar ************"
 echo "*******************************"
 
-WORKSPACE=/var/lib/jenkins/workspace/$JENKINS_JOB_NAME
-
 docker run --rm  -v $WORKSPACE:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3.8.3-openjdk-17 "$@"
