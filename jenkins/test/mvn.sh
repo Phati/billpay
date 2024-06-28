933ac90d13c84ae2f54c025125065cfb223a8944
Phati Sawant <61258100+Phati@users.noreply.github.com>
2024-06-29 01:07:36 +0530

    Update mvn.sh

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -5,4 +5,5 @@ echo "***** Running Unit Tests ******"
 echo "*******************************"
 
 echo $WORKSPACE
-docker run --rm  -v $WORKSPACE:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3.8.3-openjdk-17 "$@"
+ls $WORKSPACE
+docker run --rm  -v $WORKSPACE/:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3.8.3-openjdk-17 "$@"
