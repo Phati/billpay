dcb4c6b08e8f5870e7c8bf9037805a5242d22dc6
Phati Sawant <61258100+Phati@users.noreply.github.com>
2024-06-29 00:57:56 +0530

    Update mvn.sh

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -5,4 +5,5 @@ echo "***** Running Unit Tests ******"
 echo "*******************************"
 
 WORKSPACE=/var/lib/jenkins/workspace/$JENKINS_JOB_NAME
+echo $WORKSPACE
 docker run --rm  -v $WORKSPACE:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3.8.3-openjdk-17 "$@"
